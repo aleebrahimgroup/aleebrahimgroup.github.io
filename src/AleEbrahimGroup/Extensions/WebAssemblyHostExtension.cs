c368eddc1723a6ec3bef0f8fb0fd3a8d9a844a85
Milad Raeisi <miladsoft@yahoo.com>
2024-01-05 07:44:41 +0400

    Add languages to app

---
+++ src/AleEbrahimGroup/Extensions/WebAssemblyHostExtension.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.JSInterop;
+using System.Globalization;
+using System.Threading.Tasks;
+
+
+public static class WebAssemblyHostExtension
+{
+    public async static Task SetDefaultCulture(this WebAssemblyHost host)
+    {
+        var jsInterop = host.Services.GetRequiredService<IJSRuntime>();
+        var result = await jsInterop.InvokeAsync<string>("language.get");
+
+        CultureInfo culture;
+
+        if (result != null)
+            culture = new CultureInfo(result);
+        else
+            culture = new CultureInfo("en-US");
+
+        CultureInfo.DefaultThreadCurrentCulture = culture;
+        CultureInfo.DefaultThreadCurrentUICulture = culture;
+    }
+}
+
